71aad12ff87b6a7b06909d45cba22ebdba386a15
Francis Gutierrez <francis_gutierrez@condenast.com>
2020-01-31 09:44:03 -0400

    add react bootstrap

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "typeface-roboto";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
