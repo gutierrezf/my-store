abb9044a64cb513d04463f42e85e4d3bc3318060
Francis Gutierrez <francis_gutierrez@condenast.com>
2020-03-02 11:06:01 -0400

    update calendar func

--- client/src/containers/Home/calendar.tsx
+++ client/src/containers/Home/calendar.tsx
@@ -37,16 +37,11 @@ const Event = ({ event }: EventProps) => {
 const MyCalendar = ({ events: myEvents }: MyCalendarProps) => {
   const [date, setDate] = useState(new Date());
   const [events, setEvents] = useState(myEvents);
-  const [view, setView] = useState<View>("month");
+  const [view, setView] = useState<View>("week");
 
   const handleSelect = ({ start, end }: SlotInfo) => {
     const startDate = new Date(start.toString());
     const endDate = new Date(end.toString());
-    if (view === "month") {
-      setDate(startDate);
-      setView("day");
-      return;
-    }
 
     const title = window.prompt("New Event name");
 
@@ -64,21 +59,23 @@ const MyCalendar = ({ events: myEvents }: MyCalendarProps) => {
   };
 
   return (
-    <div style={{ height: 900 }}>
+    <div style={{ height: 1200 }}>
       <Calendar
         localizer={localizer}
         selectable
         events={events}
         view={view}
-        views={["month", "day"]}
+        views={["week", "day"]}
         onView={newView => setView(newView)}
         date={date}
         onNavigate={date => setDate(date)}
         onSelectEvent={event => alert(event.desc)}
         onSelectSlot={handleSelect}
-        timeslots={1}
+        timeslots={2}
         step={30}
         dayLayoutAlgorithm={"no-overlap"}
+        min={new Date(0, 0, 0, 7, 0, 0, 0)}
+        max={new Date(0, 0, 0, 19, 0, 0, 0)}
         components={{
           event: Event
         }}
