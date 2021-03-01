88f552bbcdf9af1dfd9ec916a7c6fa21fd04261e
Francis Gutierrez <ingutierrez.u@gmail.com>
2021-02-28 20:42:42 -0400

    update deploy

--- server/src/graphql-types/MyContext.ts
+++ server/src/graphql-types/MyContext.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from "express";
 
+interface MySession extends Express.Session {
+  userId: number // or any other type
+}
+interface MyRequest extends Request {
+  session: MySession // or any other type
+}
 export interface MyContext {
-  req: Request;
+  req: MyRequest;
   res: Response;
 }
